cabde8607ee098887a516cd48b505230a3df2160
“Alyssa <agoody44@gmail.com>
2021-02-01 20:26:29 -0500

    Adds slider animation

--- src/slider/ImageSlider.js
+++ src/slider/ImageSlider.js
@@ -26,7 +26,14 @@ const length = slides.length
     <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
     <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
         {SliderData.map((slide, idx) => {
-            return <img src={slide.image} alt='construction' className='image' />;
+            return (
+                <div className={idx === current ? 'slide active' : 'slide'} key={idx}
+                >
+                {idx === current && (
+                    <img src={slide.image} alt='construction' className='image' width='650px' height='650px' />
+                )}
+                </div>
+            );
         })}
     </section>
     );
