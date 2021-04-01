1fd4773f0dc9a672e155f4aac3448ba518f46c68
“Alyssa <agoody44@gmail.com>
2021-03-31 23:34:39 -0400

    cleans up code in social.js

--- src/contact/Social.js
+++ src/contact/Social.js
@@ -1,17 +1,13 @@
 import Insta2 from '../imgs/insta2.png';
 import Facebook from '../imgs/facebook.png';
 
-function Social (props) {
+export default function Social (props) {
     return (
     <div className="Social">
-
         <div className='Insta'>
             <img id='facebook' src={Facebook} alt="Logo" />
             <img id='insta' src={Insta2} alt="Logo" />
         </div>
-
     </div>
     )
 }
-
-export default Social;
