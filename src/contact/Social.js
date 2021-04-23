db050be75bb994c1d7db3698a623c960772837f7
“Alyssa <agoody44@gmail.com>
2021-04-22 21:27:22 -0400

    updates insta img name and facebook classname'

--- src/contact/Social.js
+++ src/contact/Social.js
@@ -1,12 +1,12 @@
-import Insta2 from '../imgs/insta2.png';
 import Facebook from '../imgs/facebook.png';
+import Insta from '../imgs/insta.png';
 
 export default function Social (props) {
     return (
     <div className="Social">
         <div className='Insta'>
-            <img id='facebook' src={Facebook} alt="Logo" />
-            <img id='insta2' src={Insta2} alt="Logo" />
+            <img className="Facebook-Logo" src={Facebook} alt="Logo" />
+            <img className="Insta-Logo" src={Insta} alt="Logo" />
         </div>
     </div>
     )
